cd123e501bf280a67c09e9069bbfd84e6bed0c52
Árni Johnsen <arni.johnsen@gmail.com>
2014-07-10 13:53:18 +0000

    Moved and renamed correlation analysis in preparation for more data sets

--- correlationAnalysis/BRCA/BRCAmethylRnaseqLm.R
+++ correlationAnalysis/BRCA/BRCAmethylRnaseqLm.R
@@ -1,4 +1,6 @@
 # Load data files
+library(doParallel)
+registerDoParallel(2)
 cat("Loading data files\n")
 if(!exists("linkedProbesGenes")){
   load("../Rdata/BRCA/linkedProbesGenes.Rdata")
@@ -26,23 +28,21 @@ allMethyl <- rbind(normalMethyl[normalSamples,],
 allRnaseq <- rbind(normalRnaseq[normalSamples,],
                    cancerRnaseq[cancerSamples,])
 }
+rm(normalMethyl, cancerMethyl, normalRnaseq, cancerRnaseq)
+gc()
 
 # Make smaller for testing
 n <- dim(allMethyl)[2]
 eps <- 1e-10
-result <- data.frame(r2 = numeric(0), 
-                     intEst  = numeric(0), sloEst  = numeric(0), 
-		     intStd  = numeric(0), sloStd  = numeric(0), 
-		     intTval = numeric(0), sloTval = numeric(0), 
-		     intPval = numeric(0), sloPval = numeric(0), 
-		     can99q  = numeric(0), can95q  = numeric(0), 
-		     canMean = numeric(0), canSd   = numeric(0), 
-		     norMean = numeric(0), norSd   = numeric(0), 
-		     norExpr = numeric(0), canExpr = numeric(0), 
-		     nonMethQuant = numeric(0))
-
 pb <- txtProgressBar(min=1, max=n, style=3)
+prt <- proc.time()
 cat("Running loop for lm\n")
+
+result <- data.frame(r2 = numeric(0), 
+                     sloEst  = numeric(0), 
+		     sloPval = numeric(0), 
+		     can99q  = numeric(0),
+		     nonMethQuant = numeric(0))
 for(i in 1:n){
   setTxtProgressBar(pb, i)
   # lm model
@@ -50,31 +50,24 @@ for(i in 1:n){
   gene  <- as.character(linkedProbesGenes$genes[i])
   x <- allMethyl[,probe]
   y <- allRnaseq[,gene] + eps
-  fit <- lm(log(y) ~ log(x))
+  fit <- lm(log(y) ~ log(x), weights=x^3)
   sum <- summary(fit)
   # 99th quantile in cancer, mean/sd in cancer, mean/sd in normal
   xc <- x[73:599]
   yc <- y[73:599]
-  result[i,] <- c(sum$r.squared, 
-                  sum$coefficients, 
-		  quantile(x[73:559], 0.99, na.rm=T), 
-		  quantile(x[73:559], 0.95, na.rm=T), 
-		  mean(x[73:559], na.rm=T), 
-		  sd(x[73:599], na.rm=T), 
-		  mean(x[1:72], na.rm=T), 
-		  sd(x[1:72], na.rm=T), 
-		  mean(y[1:72], na.rm=T),
-		  mean(y[73:599], na.rm=T), 
-		  quantile(yc[xc<0.2], 0.10, na.rm=T))
+  result[i,] <- c(sum$r.squared,                      # R^2
+                  sum$coefficients[c(2,8)],           # Slope and P-value
+		  quantile(xc, 0.99, na.rm=T),        # 99th methyl quant. of cancer
+		  quantile(yc[xc<0.2], 0.10, na.rm=T))# 10th expr. quant of non-methylated cancer
 
 }
+print(proc.time() - prt)
 cat("\n")
+colnames(result) <- c("r2", "sloEst", "sloPval", "can99q", "nonMethQuant")
 rownames(result) <- paste(linkedProbesGenes$probes[1:n], linkedProbesGenes$genes[1:n], sep="-")
 result$sloPadj <- p.adjust(result$sloPval, method="BH")
-result$intPadj <- p.adjust(result$intPval, method="BH")
 result$r <- sqrt(result$r2)*sign(result$sloEst)
 
-
 # Filter data
-save(result, "../Rdata/BRCA/lmMethylRnaseq.Rdata")
+save(result, file="../Rdata/BRCA/lmMethylRnaseq.Rdata")
 quit(save="no")
