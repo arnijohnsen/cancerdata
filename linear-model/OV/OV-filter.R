3eb742a4b6d36645e2f495f62d8fd7577ed7843b
Árni Johnsen <arni.johnsen@gmail.com>
2014-07-25 13:33:26 +0000

    Changed OV quantiles and filters to include NMP

--- linear-model/OV/OV-filter.R
+++ linear-model/OV/OV-filter.R
@@ -4,6 +4,7 @@ names <- rownames(OV.statistics)[OV.statistics$slope < 100 &
                                    OV.statistics$slope > -650 &
                                    OV.statistics$r < -0.25 &
                                    OV.statistics$p.adj < 0.01 &
+                                   OV.statistics$NMP.medians < 0.1&
                                    OV.statistics$CMP.99quant > 0.4]
 
 OV.filtered.list <- data.frame(probe = gsub("\\..*", "", names), gene = gsub(".*\\.", "", names))
--- linear-model/OV/OV-quantiles.R
+++ linear-model/OV/OV-quantiles.R
@@ -4,17 +4,22 @@ library(matrixStats)
 # Load data files
 cat("Loading data files\n")
 load("../Rdata/OV/info/OV-linked-probes-genes.Rdata")
+load("../Rdata/OV/data/OV-NMP.Rdata")
 load("../Rdata/OV/data/OV-CMP.Rdata")
 load("../Rdata/OV/data/OV-CEA.Rdata")
 
 
 # Resize matrices and use only intersecting samples
+normal.samples <- rownames(OV.NMP)
 cancer.samples <- intersect(rownames(OV.CMP), rownames(OV.CEA))
 
+OV.NMP.medians        <- colMedians(as.matrix(OV.NMP[normal.samples,]), na.rm=T)
+names(OV.NMP.medians) <- names(OV.NMP)
 OV.CMP.99quant        <- colQuantiles(as.matrix(OV.CMP[cancer.samples,]), probs=0.99, na.rm=T)
 names(OV.CMP.99quant) <- names(OV.CMP)
 
-OV.quantiles <- data.frame(CMP.99quant = OV.CMP.99quant[as.character(OV.linked.probes.genes$probes)])
+OV.quantiles <- data.frame(NMP.medians = OV.NMP.medians[as.character(OV.linked.probes.genes$probes)],
+                             CMP.99quant = OV.CMP.99quant[as.character(OV.linked.probes.genes$probes)])
 rownames(OV.quantiles)<- paste(OV.linked.probes.genes$probe,
                                  OV.linked.probes.genes$genes, sep=".")
 
