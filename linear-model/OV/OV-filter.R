950a60ff8a452f5fe47e3de4cef2d3a313932a5c
Árni Johnsen <arni.johnsen@gmail.com>
2014-07-25 10:08:15 +0000

    Added filtering script for OV linear regression results

---
+++ linear-model/OV/OV-filter.R
@@ -0,0 +1,10 @@
+load("../Rdata/OV/calc/OV-statistics.Rdata")
+
+names <- rownames(OV.statistics)[OV.statistics$slope < 100 &
+                                   OV.statistics$slope > -650 &
+                                   OV.statistics$r < -0.25 &
+                                   OV.statistics$p.adj < 0.01 &
+                                   OV.statistics$CMP.99quant > 0.4]
+
+OV.filtered.list <- data.frame(probe = gsub("\\..*", "", names), gene = gsub(".*\\.", "", names))
+write.table(OV.filtered.list, file="../Rdata/OV/calc/OV-filtered-list.txt", row.names=F, quote=F)
