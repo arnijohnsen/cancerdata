a94ccca167f0b86264613d1c19cebccf051f6f38
Árni Johnsen <arni.johnsen@gmail.com>
2014-07-28 11:18:07 +0000

    Fixed typo which broke default value value of node

--- runscript.sh
+++ runscript.sh
@@ -2,21 +2,21 @@
 
 if ! [ -z $1 ]
 then
-	echo "#!/bin/sh" >> .submission
-	echo "#" >> .submission
-	echo "#PBS -N arj32"  >> .submission
-	if [ -z $2 ]
-	then
-		echo "#PBS -l nodes=1, walltime=6:00:00" >> .submission
-	else
-		echo "#PBS -l nodes=compute-0-"$2",walltime=6:00:00" >> .submission
-	fi
-	echo  >> .submission
-	echo  >> .submission
-	echo "module add R/3.0.1" >> .submission
-	echo "cd /share/scratch/arj32/cancerdata" >> .submission
-	echo "R CMD BATCH" $1 >> .submission
+  echo "#!/bin/sh" >> .submission
+  echo "#" >> .submission
+  echo "#PBS -N arj32"  >> .submission
+  if [ -z $2 ]
+  then
+    echo "#PBS -l nodes=1,walltime=6:00:00" >> .submission
+  else
+    echo "#PBS -l nodes=compute-0-"$2",walltime=6:00:00" >> .submission
+  fi
+  echo  >> .submission
+  echo  >> .submission
+  echo "module add R/3.0.1" >> .submission
+  echo "cd /share/scratch/arj32/cancerdata" >> .submission
+  echo "R CMD BATCH" $1 >> .submission
 
-	qsub .submission
-	rm .submission
+  qsub .submission
+  rm .submission
 fi
