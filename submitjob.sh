aab852cf0d10cafffb468db53493bba6caa292d2
Árni Johnsen <arni.johnsen@gmail.com>
2014-08-20 10:49:34 +0000

    Made change to walltime in submitjob.sh

--- submitjob.sh
+++ submitjob.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 #PBS -N arj32
-#PBS -l nodes=1,walltime=03:00:00
+#PBS -l nodes=1,walltime=06:00:00
 
 
 module add R/3.0.1
 cd /share/scratch/arj32/cancerdata
-R CMD BATCH correlationAnalysis/GBM/GBMmethylRnaseqLm.R
+R CMD BATCH read-files/COAD/COAD-link-probes-genes.R
